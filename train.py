e52769dc368c7a54e621a2f2addb06832df35843
zyz <zhaoyangzheng1999@gmail.com>
2026-02-14 12:26:11 +0800

    add exp2 result

--- train.py
+++ train.py
@@ -24,14 +24,15 @@ def base_config():
         "exp_time": exp_time,
         "GPU_model": GPU_model,
         "device": "cuda" if torch.cuda.is_available() else "cpu",
-        "exp_name": "exp2_448_full",
+        "exp_name": "exp2_5090_448_full",
         "model_name": "YOLOv2",
         "save_interval": 10,
         # dataset config
         # D:\1AAAAAstudy\python_base\pytorch\all_dataset\YOLOv1_dataset
         # /root/autodl-tmp/dataset_full/YOLOv1_dataset/train
-        "train_path": r"/root/autodl-tmp/dataset_full/YOLOv1_dataset/train",
-        "test_path": r"/root/autodl-tmp/dataset_full/YOLOv1_dataset/test",
+        # /root/autodl-tmp/YOLOv1_dataset
+        "train_path": r"/root/autodl-tmp/YOLOv1_dataset/train",
+        "test_path": r"/root/autodl-tmp/YOLOv1_dataset/test",
         "anchors_json": r"dataset/anchors_k5.json",
         "stride": 32,
         
@@ -40,7 +41,7 @@ def base_config():
         "debug_mode": None, # 当debug_mode为None时,表示正常模式; 否则为debug模式,使用部分数据训练
         "num_classes": 20,
         "input_size": 448,
-        "batch_size": 16,
+        "batch_size": 64,
         "epochs": 160,
         "metric_interval": 5, # 每间隔几轮评估一次
         "num_workers": 8,
